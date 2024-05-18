2a77d3a08fe68a4049eabaafa9c6c1df81e8c15f
JoanGeertsen <ttlocuelors@gmail.com>
2024-05-18 10:49:45 -0300

    Se añadió precio

--- DD Parcial/Producto.cs
+++ DD Parcial/Producto.cs
@@ -10,20 +10,22 @@ namespace DD_Parcial
     internal abstract class Producto
     {
         #region Atributos
-        protected string _Nombre;   
+        protected string _Nombre;
+        protected double _Precio;
         protected string _FechaIncorporacion;
         protected int _Stock;
         public static string FechaInicial="01/01/2000";
         #endregion
 
         #region Constructores
-        public Producto() { _Nombre = ""; _FechaIncorporacion = FechaInicial; _Stock = 0;}
+        public Producto() { _Nombre = ""; _FechaIncorporacion = FechaInicial; _Stock = 0; _Precio = 0; }
 
-        public Producto(string _Nombre, string _FechaIncorporacion, int _Stock)
+        public Producto(string _Nombre, string _FechaIncorporacion, int _Stock, double _Precio)
         {
-            this._Nombre = (_Nombre != null) ? _Nombre: "";            
+            this._Nombre = (_Nombre != null) ? _Nombre: "";
             this._FechaIncorporacion = (_FechaIncorporacion != null) ? _FechaIncorporacion : "";
             this._Stock = (_Stock > 0) ? _Stock : 0;
+            this._Precio = (_Precio > 0) ? _Precio : 0;
         }
         #endregion
 
@@ -44,6 +46,12 @@ namespace DD_Parcial
         {
             get { return _Stock; }
             set { _Stock = (value > 0) ? value : 0; }
+        }
+
+        public double Precio
+        {
+            get { return _Precio; }
+            set { _Precio = (value > 0) ? value : 0; }
         }
         #endregion      
     }
