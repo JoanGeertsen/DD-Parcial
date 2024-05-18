9d2a30617cc786ca0c499136d60ee5c4e2792cae
JoanGeertsen <ttlocuelors@gmail.com>
2024-05-18 00:30:03 -0300

    Clase Infusion

---
+++ DD Parcial/Infusion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DD_Parcial
+{
+    internal class Infusion: Producto
+    {
+        #region Atributos
+        private string _Descripcion;
+        #endregion
+
+        #region Constructores
+        public Infusion() : base() { _Descripcion = ""; }
+
+        public Infusion(string _Nombre, string _FechaIncorporacion, int _Stock, string _Descripcion) : base(_Nombre, _FechaIncorporacion, _Stock)
+        {
+            this._Descripcion = (_Descripcion != null && _Descripcion.Trim() != "") ? _Descripcion : "";
+        }
+        #endregion
+
+        #region Property        
+        public string Descripcion
+        {
+            get { return _Descripcion; }
+            set { _Descripcion = (value != null && value.Trim() != "") ? value : ""; }
+        }
+        #endregion      
+    }
+}
