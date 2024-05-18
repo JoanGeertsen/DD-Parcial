14ff8e3943d63c19f437460b2be4253849313f9a
JoanGeertsen <ttlocuelors@gmail.com>
2024-05-18 01:03:57 -0300

    Clses Filtro y Espresso

---
+++ DD Parcial/Espresso.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DD_Parcial
+{
+    internal class Espresso: Cafe
+    {
+        #region Atributos
+        public static string MetodosRecomendados = "Espresso, Moka";
+        #endregion
+
+        #region Constructores      
+        public Espresso() : base() { }
+
+        public Espresso(string _Nombre, string _FechaIncorporacion, int _Stock, string _Tueste, string _Origen, bool _Molido)
+            : base(_Nombre, _FechaIncorporacion, _Stock, _Tueste, _Origen, _Molido) { }
+        #endregion
+    }
+}
---
+++ DD Parcial/Filtro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DD_Parcial
+{
+    internal class Filtro: Cafe
+    {
+        #region Atributos
+        public static string MetodosRecomendados = "V60, Chemex, Prensa Francesa, Aeropress";
+        #endregion
+
+        #region Constructores      
+        public Filtro() : base() { }
+
+        public Filtro(string _Nombre, string _FechaIncorporacion, int _Stock, string _Tueste, string _Origen, bool _Molido) 
+            : base(_Nombre, _FechaIncorporacion, _Stock, _Tueste, _Origen, _Molido) { }   
+        #endregion
+    }
+}
